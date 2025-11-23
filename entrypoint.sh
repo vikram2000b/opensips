e5e5c54f8fa86aec701938361c276efe85abcacf
vikram2000b <vikram@sagepilot.ai>
2025-11-23 15:51:10 +0530

    Update entrypoint script and OpenSIPS configuration for improved logging
    - Modified entrypoint script to remove the -FE flag from the OpenSIPS command.
    - Changed logging configuration in opensips.cfg from log_stderror to stderror_enabled for clarity.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ echo "  MEDIA_SERVER_HOST: ${MEDIA_SERVER_HOST}"
 echo "  MEDIA_SERVER_PORT: ${MEDIA_SERVER_PORT:-80}"
 
 # Start OpenSIPS
-exec opensips -f /etc/opensips/opensips.cfg -FE
+exec opensips -f /etc/opensips/opensips.cfg -F
